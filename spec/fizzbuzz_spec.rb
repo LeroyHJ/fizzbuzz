a432c40dcf872b3ecbf1c28948b4126b3b0e53c0
sunnymak <sunnymakcc@hotmail.com>
2022-08-10 22:27:29 +0100

    completed rspec and code

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,4 +3,36 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+
+  it 'returns "fizz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+
+  it 'returns "fizz" when passed 6' do
+    expect(fizzbuzz(6)).to eq 'fizz'
+  end
+
+  it 'returns "fizz" when passed 9' do
+    expect(fizzbuzz(9)).to eq 'fizz'
+  end
+
+  it 'returns "fizz" when passed 10' do
+    expect(fizzbuzz(10)).to eq 'buzz'
+  end
+
+  it 'returns "fizz" when passed 12' do
+    expect(fizzbuzz(12)).to eq 'fizz'
+  end
+
+  it 'returns "fizz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+
+  it 'returns "fizz" when passed 18' do
+    expect(fizzbuzz(18)).to eq 'fizz'
+  end
+
+  it 'returns "fizz" when passed 20' do
+    expect(fizzbuzz(20)).to eq 'buzz'
+  end
 end
